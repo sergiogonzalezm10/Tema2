477a2131da8127f30ed2a381cfbb252371666d74
sergio.gonzalezm <sergio.gonzalezm@106-31.info2>
2025-10-20 09:36:34 +0200

    si

--- src/tema2/Ejercicio3.java
+++ src/tema2/Ejercicio3.java
@@ -9,6 +9,10 @@ public class Ejercicio3 {
 		double b;
 		double c;
 		double x;
+		double x1;
+		double x2;
+		double raiz;
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Introduce valor a: ");
@@ -17,8 +21,24 @@ public class Ejercicio3 {
 		b = sc.nextDouble();
 		System.out.println("Introduce valor c: ");
 		c = sc.nextDouble();
-
-		x= (-b+-())
+		
+		raiz = b*b - 4*a*c;
+		
+		if (raiz > 0 )  {
+		x1 = (-b + Math.sqrt(raiz))/ (2 * a);
+		x2 = (-b - Math.sqrt(raiz))/ (2 * a);
+		System.out.println("Soluciones reales:");
+		System.out.println("X = " + x1);
+		System.out.println("X = " + x2);
+	
+		} else if (raiz == 0) {
+        x = -b / (2 * a);
+        System.out.println("Hay una única solución real:");
+        System.out.println("x = " + x);
+        
+		} else {
+			System.out.println("La ecuación no tiene soluciones reales.");
+		}
+		}
 	}
 
-}
