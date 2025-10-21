23a38b00a3292f09d3b8ac64d62f27ce24b5e125
sergio.gonzalezm <sergio.gonzalezm@106-31.info2>
2025-10-21 13:47:43 +0200

    si

---
+++ src/conditionals3/Ejercicio4.java
@@ -0,0 +1,20 @@
+package conditionals3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		
+		Random numero = new Random();
+		
+		double num = numero.nextDouble(1,100);
+		double num2 = numero.nextDouble(1,100);
+		
+		System.out.println("Nota incorrecta ( 0 al 10 )");
+
+	}
+
+}
