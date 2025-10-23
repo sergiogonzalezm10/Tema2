e50c57db6aa43fc83c361b326ea3fb374b025572
sergio.gonzalezm <sergio.gonzalezm@106-22.info2>
2025-10-23 14:39:20 +0200

    si

--- src/conditionals3/Ejercicio6.java
+++ src/conditionals3/Ejercicio6.java
@@ -17,9 +17,9 @@ public class Ejercicio6 {
 		System.out.println("Introduce el año: ");
 		año = sc.nextInt();
 		
-		bisiesto = ( año % 4  == 0 ) || (año % 100 == 0) || ( (año % 100 == 0) && (año % 400 == 0));
+		bisiesto = (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
 		
-		if (bisiesto = true ) {
+		if (bisiesto == true) {
 			System.out.println("Tu año es bisiesto");
 			
 			switch (mes) {
@@ -48,25 +48,43 @@ public class Ejercicio6 {
 			} case 12 -> {
 				System.out.println("Diciembre: 31 días");
 			} default -> {
-				System.out.println("El año tiene 12 meses");
+				System.out.println("¡El año tiene 12 meses!");
 						 }
 			}
 			
 		} else {
 			System.out.println("Tu año no es bisiesto");
-			System.out.println("Enero: 31 días");
-			System.out.println("Febrero: 2 días");
-			System.out.println("Marzo: 31 días");
-			System.out.println("Abril: 30 días");
-			System.out.println("Mayo: 31 días");
-			System.out.println("Junio: 30 días");
-			System.out.println("Julio: 31 días");
-			System.out.println("Agosto: 31 días");
-			System.out.println("Septiembre: 30 días");
-			System.out.println("Octubre: 31 días");
-			System.out.println("Noviembre: 30 días");
-			System.out.println("Diciembre: 31 días");
-		}
+			
+			switch (mes) {	
+			case 1 -> {
+				System.out.println("Enero: 31 días");
+			} case 2 -> {
+				System.out.println("Febrero: 28 días");
+			} case 3 -> {
+				System.out.println("Marzo: 31 días");
+			} case 4 -> {
+				System.out.println("Abril: 30 días");
+			} case 5 -> {
+				System.out.println("Mayo: 31 días");
+			} case 6 -> {
+				System.out.println("Junio: 30 días");
+			} case 7 -> {
+				System.out.println("Julio: 31 días");
+			} case 8 -> {
+				System.out.println("Agosto: 31 días");
+			} case 9 -> {
+				System.out.println("Septiembre: 30 días");
+			} case 10 -> {
+				System.out.println("Octubre: 31 días");
+			} case 11 -> {
+				System.out.println("Noviembre: 30 días");
+			} case 12 -> {
+				System.out.println("Diciembre: 31 días");
+			} default -> {
+				System.out.println("¡El año tiene 12 meses!");
+						 }
+			}
 	}
-
+	}
 }
+
