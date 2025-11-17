0b7edfd6ac2b57df4d28b9b94a15951fad8e86a2
sergiogonzalezm10 <sergio.gonzalezm@iesnervion.es>
2025-11-17 10:01:58 +0100

    si

--- src/buclefor/Ejercicio7.java
+++ src/buclefor/Ejercicio7.java
@@ -5,34 +5,41 @@ import java.util.Scanner;
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-
+		
+				// Variable para almacenar el número introducido
 		        int numero;
+		        //Variable para indicar si el número es primo
 		        boolean esPrimo = true;
-		        		
+		        
+		        //Inicio escáner
 		        Scanner sc = new Scanner(System.in);
 		        
+		        
+		        //Pedimos al usuario que introduzca un número
 		        System.out.print("Introduce un número entero positivo: ");
 		        numero = sc.nextInt();
 
-		        // Un número menor o igual a 1 no es primo
+		        // Si un número menor o igual a 1 no es primo.
 		        if (numero <= 1) {
 		            esPrimo = false;
 		        } else {
-		            // Comprobar divisores desde 2 hasta numero-1
+		        	 
+		        	// Comprobamos si el número es primo
 		            for (int i = 2; i < numero; i++) {
 		                if (numero % i == 0) {
 		                    esPrimo = false;
-		                    break;
 		                }
 		            }
 		        }
 
+		        // Imprimimos el resultado
 		        if (esPrimo) {
 		            System.out.println(numero + " es primo.");
 		        } else {
 		            System.out.println(numero + " NO es primo.");
 		        }
-
+		        
+		        //Cierre escáner
 		        sc.close();
 		    }
 		}
